dbb034c915665f51fd5c144127c7f2d1de39cb70
UnaL <unalil539@gmail.com>
2021-02-05 00:35:16 +0200

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> sehirPlaka = new Dictionary<int,string>();
+
+            sehirPlaka.Add(16, "Bursa");
+            sehirPlaka.Add(75, "Ardahan");
+            sehirPlaka.Add(67, "Zonguldak");
+            sehirPlaka.Add(34, "İstanbul");
+            foreach (var item in sehirPlaka)
+            {
+                Console.WriteLine(item+"Başarıyla eklendi");
+            }
+        }
+    }
+}
